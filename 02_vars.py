139b048bcb3e066d16922383d09ac17be8007cc0
Jose Villarroel <josedelvallevs@gmail.com>
2023-05-02 22:52:34 -0500

    file 02_vars.py

---
+++ 02_vars.py
@@ -0,0 +1,20 @@
+print("Hola, vars")
+
+# Esto es una variable
+my_name = 'Nicolas'
+print(my_name)
+
+# Esto es variable con un número
+my_age = 54
+print(my_age)
+
+my_name = "Santiago"
+print("aqui cambio ",my_name)
+
+# input
+
+my_name = input("Cuál es tu nombre? ")
+print('Usando Input my_name es: ', my_name)
+
+my_age = input("Cuál es tu edad? ")
+print('Usando Input my_age es: ', my_age)
