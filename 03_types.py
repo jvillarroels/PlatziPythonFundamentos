2e13509f6f0ad351dea87cbf363e4ed557cc07f8
Jose Villarroel <josedelvallevs@gmail.com>
2023-05-02 23:06:25 -0500

    Add file 03_types.py

---
+++ 03_types.py
@@ -0,0 +1,21 @@
+# string
+my_name = "Nicolas"
+my_name = 'Santiago'
+print('my_name => ', my_name)
+print(type(my_name))
+
+# int
+my_age = 54
+print('my_age => ', my_age)
+print(type(my_age))
+
+# boolean
+#is_single = True
+is_single = False
+print('is_single => ', is_single)
+print(type(is_single))
+
+# inputs
+my_age = input('Cual es tu edad? => ')
+print('my_age => ', my_age)
+print(type(my_age))
